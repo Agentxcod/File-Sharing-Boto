ca35781b49855729c7b09cffda0b5ad1c1651bbc
Agentxcod <filesharingbotbyagent@gmail.com>
2024-07-24 20:11:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -84,6 +84,14 @@ async def start_command(client: Client, message: Message):
             except:
                 pass
         return 
+        reply_markup = InlineKeyboardMarkup(
+            [        
+                [
+                    
+                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                ]
+            ]
+        )
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
