91a669a4720c2a4344926e1793c6fb3f7d557c3c
Agentxcod <filesharingbotbyagent@gmail.com>
2024-07-24 19:53:27 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -113,9 +113,7 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
 async def not_joined(client: Client, message: Message):
     buttons = [ 
         [
-           InlineKeyboardButton(
-                "Join Channel",
-                url = "https://t.me/+-weIgcLJ1M45MzVk"),
+
             InlineKeyboardButton(
                 "Join Channel",
                 url = client.invitelink)
