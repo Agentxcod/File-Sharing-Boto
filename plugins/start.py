0f013b4cff958c33a51cafa7f3df9645bffb538c
Agentxcod <filesharingbotbyagent@gmail.com>
2024-07-25 08:14:51 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -83,11 +83,11 @@ async def start_command(client: Client, message: Message):
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
             except:
                 pass
-        return 
+        return
+    else:
         reply_markup = InlineKeyboardMarkup(
-            [        
+            [          
                 [
-                    
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
             ]
@@ -105,6 +105,7 @@ async def start_command(client: Client, message: Message):
             quote = True
         )
         return
+        
 
     
 #=====================================================================================##
